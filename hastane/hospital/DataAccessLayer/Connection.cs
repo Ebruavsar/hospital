98b03fb0e42fb424e26a52de76f0f6bb68f8e799
Ebruavsar <73585933+Ebruavsar@users.noreply.github.com>
2024-03-20 20:29:16 +0300

    veritabanını dinamik yapmak için

--- hastane/hospital/DataAccessLayer/Connection.cs
+++ hastane/hospital/DataAccessLayer/Connection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,22 @@ namespace DataAccessLayer
 {
     public class Connection
     {
-       
-            public OleDbConnection ConnectionOpen()
+
+        private static string GetDatabasePath()
+        {
+
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string dbPath = Path.Combine(appDirectory, "hospital.accdb");
+            return dbPath;
+        }
+
+        public OleDbConnection ConnectionOpen()
             {
-                OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\ebrua\\Desktop\\hastane\\hospital.accdb;");
-                connection.Open();
-                return connection;
-            }
+            string connectionString = $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={GetDatabasePath()}";
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
         
     }
 }
